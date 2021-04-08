eff5689cfac55a1877d078a08cd910e4a44be23f
Nomar <nomarlonon1@gmail.com>
2021-04-08 20:15:26 +0800

    just updated Client Area Functionality

--- app/assets/scripts/modules/ClientArea.js
+++ app/assets/scripts/modules/ClientArea.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import Axios from 'axios'
 
 class ClientArea {
     constructor() {
@@ -22,7 +22,7 @@ class ClientArea {
             this.form.remove()
             this.contentArea.innerHTML = response.data
         }).catch(() => {
-            this.contentArea.innerHTML = `<p class="client-area__error">That secret phrase is not correct. Try again.</p>`
+            this.contentArea.innerHTML = `<p class="client-area__error"> That secret phrase is not correct. Try again.</p>`
             this.field.value = ''
             this.field.focus()
         })
